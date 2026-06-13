587cca10f62ba18a00cecd8091abfa122808a185
Niveditha Ragesh <rageshniveditha@gmail.com>
2026-06-13 13:15:24 +0530

    panic screen done

---
+++ frontend/app.js
@@ -0,0 +1,17 @@
+document
+.getElementById("panicBtn")
+.onclick=()=>{
+
+document.body.innerHTML=`
+
+<h1>Select Incident</h1>
+
+<button>OTP Shared</button>
+
+<button>Account Hacked</button>
+
+<button>Suspicious Link</button>
+
+`;
+
+};
