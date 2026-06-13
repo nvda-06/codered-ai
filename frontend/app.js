2ed55105c2d0b141a1d19cc6d491009eec689e9e
Niveditha Ragesh <rageshniveditha@gmail.com>
2026-06-13 13:27:08 +0530

    added recovery screen

--- frontend/app.js
+++ frontend/app.js
@@ -6,12 +6,50 @@ document.body.innerHTML=`
 
 <h1>Select Incident</h1>
 
-<button>OTP Shared</button>
+<button class="incident">
+OTP Shared
+</button>
 
-<button>Account Hacked</button>
+<button class="incident">
+Account Hacked
+</button>
 
-<button>Suspicious Link</button>
+<button class="incident">
+Suspicious Link
+</button>
 
 `;
 
 };
+
+
+
+document.addEventListener("click",(e)=>{
+
+if(
+e.target.classList.contains("incident")
+){
+
+document.body.innerHTML=`
+
+<h1>Threat Level: HIGH</h1>
+
+<h2>Recovery Steps</h2>
+
+<button>
+Lock Account
+</button>
+
+<button>
+Change Password
+</button>
+
+<button>
+Enable MFA
+</button>
+
+`;
+
+}
+
+});
